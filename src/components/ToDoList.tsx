e29acdf5d8f53ab6c299c3a5c23ef88d51e0d437
AARONJ <seungwoo.aaron@gmail.com>
2023-06-15 01:53:51 +0900

    selector - using get to recategorize todos

--- src/components/ToDoList.tsx
+++ src/components/ToDoList.tsx
@@ -1,19 +1,35 @@
 import { useRecoilValue } from "recoil";
 import CreateToDo from "./CreateToDo";
-import { toDoState } from "../atoms";
+import { toDoSelector, toDoState } from "../atoms";
 import ToDo from "./ToDo";
 
 function ToDoList() {
-    const toDos = useRecoilValue(toDoState);
+    // const toDos = useRecoilValue(toDoState);
+    const [toDos, doings, dones] = useRecoilValue(toDoSelector);
 
     return (
         <div>
             <h1>To Dos</h1>
             <hr />
             <CreateToDo />
+            <h2>To Do</h2>
             <ul>
                 {toDos.map((toDo) => (
                     <ToDo key={toDo.id} {...toDo} />
+                ))}
+            </ul>
+            <hr />
+            <h2>Doing</h2>
+            <ul>
+                {doings.map((doing) => (
+                    <ToDo key={doing.id} {...doing} />
+                ))}
+            </ul>
+            <hr />
+            <h2>Done</h2>
+            <ul>
+                {dones.map((done) => (
+                    <ToDo key={done.id} {...done} />
                 ))}
             </ul>
         </div>
