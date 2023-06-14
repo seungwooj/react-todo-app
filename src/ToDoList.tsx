f478404815ba8134c88ea559ebff4da209c6023d
AARONJ <seungwoo.aaron@gmail.com>
2023-06-14 17:22:36 +0900

    validation without react-hook-form

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -2,14 +2,19 @@ import { useState } from "react";
 
 function ToDoList() {
     const [toDO, setToDo] = useState("");
+    const [toDoError, setToDoError] = useState("");
     const onChange = (event: React.FormEvent<HTMLInputElement>) => {
         const {
             currentTarget: { value },
         } = event;
+        setToDoError("");
         setToDo(value);
     };
     const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault();
+        if (toDO.length < 10) {
+            setToDoError("toDo should be longer");
+        }
         console.log(toDO);
     };
     return (
@@ -21,6 +26,7 @@ function ToDoList() {
                     placeholder="Write a to do"
                 />
                 <button>Add</button>
+                {toDoError !== "" ? toDoError : null}
             </form>
         </div>
     );
