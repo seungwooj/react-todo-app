b1f0817c5141264eb7e3530944a888eceac19af4
AARONJ <seungwoo.aaron@gmail.com>
2023-06-14 22:39:21 +0900

    ToDoList: setup

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -1,105 +1,23 @@
 import { useForm } from "react-hook-form";
 
 interface IForm {
-    email: string;
-    firstName: string;
-    lastName: string;
-    password: string;
-    password1: string;
-    extraError?: string;
+    toDo: string;
 }
 
 function ToDoList() {
-    // useForm : register, handleSubmit 사용가능
-    // register : state과 동일, form의 input에 등록, validation내용 설정
-    // handleSubmit : form의 onSubmit에 등록, onValid fn을 필수 arg로 받는다.
-    // formState : form의 errors객체를 쓸 수 있음. (에러 내용 확인 가능.)
-    // setError: error내용 설정 가능
-    const {
-        register,
-        handleSubmit,
-        formState: { errors },
-        setError,
-    } = useForm<IForm>();
-
-    // onValid fn : called if the data is valid
-    const onValid = (data: IForm) => {
-        if (data.password !== data.password1) {
-            return setError("password1", {
-                message: "Password are not the same",
-            });
-        }
-        // return setError("extraError", { message: "Server offline" });
+    const { register, handleSubmit, setValue } = useForm<IForm>();
+    const handleValid = (data: IForm) => {
+        console.log("add to do", data.toDo);
+        setValue("toDo", "");
     };
-
     return (
         <div>
-            <form
-                style={{ display: "flex", flexDirection: "column" }}
-                onSubmit={handleSubmit(onValid)}
-            >
+            <form onSubmit={handleSubmit(handleValid)}>
                 <input
-                    {...register("email", {
-                        required: "email is required",
-                        pattern: {
-                            value: /^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/g,
-                            message: "Invalid email",
-                        },
-                    })}
-                    placeholder="Write your Email"
+                    {...register("toDo", { required: "Please write a To Do" })}
+                    placeholder="Write a todo"
                 ></input>
-                <span>{errors?.email?.message}</span>
-                <input
-                    {...register("firstName", {
-                        required: "first name is required",
-                        minLength: {
-                            value: 5,
-                            message: "Your first name is too short",
-                        },
-                        validate: {
-                            noSpace: (value) =>
-                                value.includes(" ") ? "no space allowed" : true,
-                        },
-                    })}
-                    placeholder="Write your First Name"
-                ></input>
-                <span>{errors?.firstName?.message as string}</span>
-                <input
-                    {...register("lastName", {
-                        required: "last name is required",
-                        minLength: {
-                            value: 2,
-                            message: "Your last name is too short",
-                        },
-                    })}
-                    placeholder="Write your Last Name"
-                ></input>
-                <span>{errors?.lastName?.message as string}</span>
-                <input
-                    {...register("password", {
-                        required: "password is required",
-                        minLength: {
-                            value: 10,
-                            message: "Your password is too short",
-                        },
-                    })}
-                    placeholder="Write your Password"
-                ></input>
-                <span>{errors?.password?.message as string}</span>
-
-                <input
-                    {...register("password1", {
-                        required: "password1 is required",
-                        minLength: {
-                            value: 10,
-                            message: "Your password1 is too short",
-                        },
-                    })}
-                    placeholder="Write your Password again"
-                ></input>
-                <span>{errors?.password1?.message as string}</span>
                 <button>Add</button>
-                <span>{errors?.extraError?.message}</span>
             </form>
         </div>
     );
